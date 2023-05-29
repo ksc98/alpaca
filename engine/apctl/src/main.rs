4998190230f37fe701986079ede4c4ccbf547bc7
Kyle Chang <kylechang96@gmail.com>
2023-05-29 03:51:34 -0700

    update apctl

--- engine/apctl/src/main.rs
+++ engine/apctl/src/main.rs
@@ -1,9 +1,13 @@
-use chrono;
+use chrono::Utc;
+use chrono_english::{parse_date_string, Dialect};
+
 use clap::Parser;
+
 use engine::{engine_client::EngineClient, BuyRequest};
+
 #[macro_use]
 extern crate log;
-use log::Level;
+extern crate date_time_parser;
 
 // Alpaca cli
 #[derive(Parser, Debug)]
@@ -22,8 +26,8 @@ struct Args {
     value: i32,
 
     // The time the order should be submitted at
-    #[arg(long, default_value_t = chrono::offset::Utc::now().timestamp())]
-    time: i64,
+    #[arg(long, default_value_t = String::from("now"))]
+    time: String,
 
     // The exchange the order should be placed on
     #[arg(short, long, default_value_t = String::from("alpaca"))]
@@ -39,15 +43,35 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
     let args = Args::parse();
     let mut client = EngineClient::connect("http://0.0.0.0:4321").await?;
+    let ts = parse_time(&args.time);
+    let qv = match args.quantity > 0 {
+        true => Some(crate::engine::buy_request::Qv::Quantity(args.quantity)),
+        false => Some(crate::engine::buy_request::Qv::Value(args.value)),
+    };
     let request = tonic::Request::new(BuyRequest {
         ticker: args.ticker.to_string(),
-        value: args.value,
-        quantity: args.quantity,
+        qv,
         exchange: args.exchange,
-        time: args.time,
+        time: ts,
     });
 
-    let response = client.buy(request).await?;
-    println!("Response from server: \n{}", response.into_inner().message);
+    // let response: Response<BuyResponse> = Response;
+    if ts <= chrono::offset::Utc::now().timestamp() {
+        let response = client.buy(request).await?;
+        println!("Response from server: \n{}", response.into_inner().message);
+    } else {
+        let response = client.schedule(request).await?;
+        println!("Response from server: \n{}", response.into_inner().message);
+    }
     Ok(())
+}
+
+fn parse_time(expression: &str) -> i64 {
+    match parse_date_string(expression, Utc::now(), Dialect::Us) {
+        Ok(time) => time.timestamp(),
+        Err(e) => {
+            println!("Error parsing time: {}", e);
+            0
+        }
+    }
 }
